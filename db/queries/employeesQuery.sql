f31db2697dd1095591f70242a373c7ffd4b09864
Ren Besson <ren.besson@outlook.com>
2023-01-30 00:08:23 -0400

    Left Outer Join used to include null columns

--- db/queries/employeesQuery.sql
+++ db/queries/employeesQuery.sql
@@ -1,6 +1,6 @@
 -- Query Employees --
 SELECT
-        employees.id AS "Employee ID",
+		employees.id AS "Employee ID",
         employees.first_name AS "First Name",
         employees.last_name AS "Last Name",
         roles.title AS "Job Title",
@@ -8,6 +8,6 @@ SELECT
         roles.salary AS Salary,
         CONCAT(managers.first_name, " ", managers.last_name) AS Manager
     FROM employees
-    JOIN roles ON role_id = roles.id
-    JOIN departments ON roles.department_id = departments.id
+    LEFT OUTER JOIN roles ON role_id = roles.id
+    LEFT OUTER JOIN departments ON roles.department_id = departments.id
     INNER JOIN employees managers ON managers.id = employees.manager_id;
